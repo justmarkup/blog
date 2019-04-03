191ba04fdfad68c2b316aee1e3a34607e47b5b4f
Michael Scharnagl <hallo@justmarkup.com>
2019-04-03 08:24:57 +0200

    #4 - add skip links

--- .eleventy.js
+++ .eleventy.js
@@ -41,6 +41,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
 
     eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("js");
 
     /* Markdown Plugins */
     let markdownIt = require("markdown-it");
---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('skiplink').addEventListener('click', function(e) {
+    var target = document.querySelectorAll('h1, h2, h3, h4, h5, h6')[0];
+    target.setAttribute('tabindex', "-1");
+    target.focus();
+
+    e.preventDefault();
+});
