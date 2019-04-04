3d5982967a7d96614ef8fe3d79d20e080a006c50
Michael Scharnagl <hallo@justmarkup.com>
2019-04-04 08:27:38 +0200

    # - add twitter share link enhanced as web share

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* *fix* the skiplink, see also: https://axesslab.com/skip-links/*/
 document.getElementById('skiplink').addEventListener('click', function(e) {
     var target = document.querySelectorAll('h1, h2, h3, h4, h5, h6')[0];
     target.setAttribute('tabindex', "-1");
