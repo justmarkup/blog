99449d225ae6db8ec7de1f798f2384cece9af5ea
Michael Scharnagl <hallo@justmarkup.com>
2021-01-16 20:26:47 +0100

    WIP: Fetch data from Pocket to show in reading list

---
+++ _data/pocket.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+require('dotenv').config()
+
+const CONSUMBER_KEY = process.env.POCKET_CONSUMER_KEY;
+const ACCESS_TOKEN = process.env.POCKET_ACCESS_TOKEN;
+const TAG = process.env.POCKET_TAG;
+const POCKET_URI = `https://getpocket.com/v3/get?consumer_key=${CONSUMBER_KEY}&access_token=${ACCESS_TOKEN}&tag=${TAG}`
+
+module.exports = async function() {
+    console.log("Fetching pocket list…");
+
+    return fetch(POCKET_URI)
+        .then(res => res.json())
+        .then(json => {
+            return Object.values(json.list);
+        });
+};
